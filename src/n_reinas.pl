8b41ae7c3411ac9e933326e35ca05d5edce87356
Demian Ferreiro <epidemian@gmail.com>
2011-05-09 14:08:47 +0000

    Ahí le saqué la recursividad infinita... agregándole un "!" nomás...
    git-svn-id: http://tdl-prolog.googlecode.com/svn/trunk@15 983ddbaf-cc9a-4c94-2610-5b308a8b8364

--- src/n_reinas.pl
+++ src/n_reinas.pl
@@ -1,4 +1,4 @@
-generar(0, []).
+generar(0, []) :- !.
 generar(N, [N|L]) :- N1 is N - 1, generar(N1, L).
 
 reina_no_choca(Col, Cols) :- reina_no_choca(Col, 1, Cols).
@@ -21,13 +21,13 @@ insertar(X, [Y|Ys], [Y|Zs]) :- insertar(X, Ys, Zs).
 
 solucion(N, Tablero) :- generar(N, Inicial), permutacion(Inicial, Tablero), tablero_valido(Tablero).
 
+% Código fiero para imprimir el tablero.
+imprimir_tablero(_,[]).
+imprimir_tablero(N, [Col|Cols]) :- imprimir_fila(N, Col), nl, imprimir_tablero(N, Cols).
+imprimir_fila(N, Col_reina) :- imprimir_fila(N, 1, Col_reina).
+imprimir_fila(N, Col,_) :- Col > N, !.
+imprimir_fila(N, Col, Col_reina) :- imprimir_casillero(Col, Col_reina), Col1 is Col + 1, imprimir_fila(N, Col1, Col_reina).
+imprimir_casillero(Col_reina, Col_reina) :- !,print('R').
+imprimir_casillero(_, _) :- print('-').
 
-%imprimir_tablero(_,[]).
-%imprimir_tablero(N, [Col|Cols]) :- imprimir_fila(N, Col), nl, imprimir_tablero(N, Cols).
-%imprimir_fila(N, Col_reina) :- imprimir_fila(N, 1, Col_reina).
-%imprimir_fila(N, Col, Col_reina) :- Col =< N, imprimir_casillero(Col, Col_reina), Col1 is Col + 1, imprimir_fila(N, Col1, Col_reina).
-%imprimir_fila(_,_,_).
-%imprimir_casillero(Col_reina, Col_reina) :- !,print(r).
-%imprimir_casillero(_, _) :- print(o).
-
-%imprimir_solucion(N) :- solucion(N, T), imprimir_tablero(N, T), nl.
+imprimir_solucion(N) :- solucion(N, T), imprimir_tablero(N, T), nl.
