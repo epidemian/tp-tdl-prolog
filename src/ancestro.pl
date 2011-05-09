dd6da23fad681ee54f96ceceb898857695dde305
Demian Ferreiro <epidemian@gmail.com>
2011-05-09 19:53:09 +0000

    Avances varios
    git-svn-id: http://tdl-prolog.googlecode.com/svn/trunk@18 983ddbaf-cc9a-4c94-2610-5b308a8b8364

--- src/ancestro.pl
+++ src/ancestro.pl
@@ -8,8 +8,7 @@ padre(zeus, artemisa).
 
 % Reglas sencillas.
 abuelo(A, B) :- padre(A, X), padre(X, B).
-hermanos(A, B) :- padre(X, A), padre(X, B), A \= B.
+hermano(A, B) :- padre(X, A), padre(X, B), A \= B.
 
-% El OR no es necesario.
 ancestro(A, B) :- padre(A, B).
 ancestro(A, B) :- padre(A, X), ancestro(X, B).
